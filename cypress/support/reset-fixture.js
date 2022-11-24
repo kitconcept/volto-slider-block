1273bff515640a602950f412847a093748c33fba
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-11-24 16:47:42 +0100

    Prettier

--- cypress/support/reset-fixture.js
+++ cypress/support/reset-fixture.js
@@ -4,7 +4,8 @@ function setup() {
     method: 'POST',
     url: `${api_url}/RobotRemote`,
     headers: { Accept: 'text/xml', 'content-type': 'text/xml' },
-    body: '<?xml version="1.0"?><methodCall><methodName>run_keyword</methodName><params><param><value><string>remote_zodb_setup</string></value></param><param><value><array><data><value><string>plone.app.robotframework.testing.PLONE_ROBOT_TESTING</string></value></data></array></value></param></params></methodCall>',
+    body:
+      '<?xml version="1.0"?><methodCall><methodName>run_keyword</methodName><params><param><value><string>remote_zodb_setup</string></value></param><param><value><array><data><value><string>plone.app.robotframework.testing.PLONE_ROBOT_TESTING</string></value></data></array></value></param></params></methodCall>',
   }).then(() => cy.log('Setting up API fixture'));
 }
 
@@ -14,7 +15,8 @@ function teardown() {
     method: 'POST',
     url: `${api_url}/RobotRemote`,
     headers: { Accept: 'text/xml', 'content-type': 'text/xml' },
-    body: '<?xml version="1.0"?><methodCall><methodName>run_keyword</methodName><params><param><value><string>remote_zodb_teardown</string></value></param><param><value><array><data><value><string>plone.app.robotframework.testing.PLONE_ROBOT_TESTING</string></value></data></array></value></param></params></methodCall>',
+    body:
+      '<?xml version="1.0"?><methodCall><methodName>run_keyword</methodName><params><param><value><string>remote_zodb_teardown</string></value></param><param><value><array><data><value><string>plone.app.robotframework.testing.PLONE_ROBOT_TESTING</string></value></data></array></value></param></params></methodCall>',
   }).then(() => cy.log('Tearing down API fixture'));
 }
 
