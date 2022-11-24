b6cad41e9da33ab6c1c5c5ff199a26f4828a250b
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-11-24 09:58:03 +0100

    Remove the custom widget and context

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import Edit from './components/Edit';
 import View from './components/View';
-import SliderObjectListWidget from './components/ObjectListWidget';
 import sliderSVG from '@plone/volto/icons/slider.svg';
 import './theme/main.less';
 
 const applyConfig = (config) => {
-  // This is a custom list widget that takes a context from
-  // The main Edit component to control the current slide (element)
-  // and syncs it with the current element in the object list.
-  config.widgets.widget.slider_object_list = SliderObjectListWidget;
-
   config.blocks.blocksConfig.slider = {
     id: 'slider',
     title: 'Slider',
