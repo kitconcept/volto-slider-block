537df5b43b4d7e72f370581f123db78f779eb26a
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-10-25 13:40:22 +0200

    Fix a11y for the slides, add a11y acceptance test.

--- acceptance/cypress/tests/block.cy.js
+++ acceptance/cypress/tests/block.cy.js
@@ -63,4 +63,47 @@ context('Block Acceptance Tests', () => {
 
     cy.get('.teaser-item-title').should('be.visible').contains('My Page');
   });
+
+  it('a11y', () => {
+    cy.intercept('PATCH', '/**').as('save');
+    cy.intercept('GET', `/**/*?expand*`).as('content');
+
+    cy.visit('/document/edit');
+    cy.addNewBlock('slider');
+
+    // First slide
+    cy.get(
+      '.objectbrowser-field[aria-labelledby="fieldset-default-field-label-href-0-slides-0"] button[aria-label="Open object browser"]',
+    ).click();
+    cy.get('aside .breadcrumbs svg.home-icon').click();
+    cy.findByLabelText('Select My Page').dblclick();
+
+    // Second slide
+    cy.findByText('Add item').click();
+    // cy.findByLabelText('Show item #2').click();
+    cy.get(
+      '.objectbrowser-field[aria-labelledby="fieldset-default-field-label-href-0-slides-1"] button[aria-label="Open object browser"]',
+    ).should('be.visible');
+    cy.get(
+      '.objectbrowser-field[aria-labelledby="fieldset-default-field-label-href-0-slides-1"] button[aria-label="Open object browser"]',
+    ).click();
+    cy.get('aside .breadcrumbs svg.home-icon').click();
+    cy.findByLabelText('Select My Page').dblclick();
+
+    cy.get('#toolbar-save').click();
+    cy.wait('@save');
+    cy.wait('@content');
+
+    cy.get('.highlight-image-wrapper img')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+
+    cy.get('.teaser-item-title').should('be.visible').contains('My Page');
+
+    cy.injectAxe();
+    cy.checkAccessibility();
+  });
 });
