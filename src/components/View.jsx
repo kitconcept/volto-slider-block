9c6dc118816a08de536ef0b80d77643790d9e124
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-11-28 17:35:40 +0100

    refine it a little bit more, fix initial state

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -5,12 +5,8 @@ import cx from 'classnames';
 import { defineMessages, useIntl } from 'react-intl';
 import Body from './Body';
 import { withBlockExtensions } from '@plone/volto/helpers';
-import { Icon } from '@plone/volto/components';
-import rightArrowSVG from '@plone/volto/icons/right-key.svg';
-import leftArrowSVG from '@plone/volto/icons/left-key.svg';
+import { DotButton, NextButton, PrevButton } from './DotsAndArrows';
 import teaserTemplate from '../icons/teaser-template.svg';
-import { SlidesWidthFix, useNodeDimensions } from '../helpers';
-import config from '@plone/volto/registry';
 
 const messages = defineMessages({
   PleaseChooseContent: {
@@ -19,78 +15,6 @@ const messages = defineMessages({
       'Please choose an existing content as source for this element',
   },
 });
-
-const DotButton = (props) => {
-  const { children, ...restProps } = props;
-
-  return (
-    <button type="button" {...restProps}>
-      {children}
-    </button>
-  );
-};
-
-export const PrevButton = (props) => {
-  const { children, ...restProps } = props;
-
-  return (
-    <button
-      className="embla__button embla__button--prev"
-      type="button"
-      {...restProps}
-    >
-      <svg className="embla__button__svg" viewBox="0 0 532 532">
-        <path
-          fill="currentColor"
-          d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
-        />
-      </svg>
-      {children}
-    </button>
-  );
-};
-
-export const NextButton = (props) => {
-  const { children, ...restProps } = props;
-
-  return (
-    <button
-      className="embla__button embla__button--next"
-      type="button"
-      {...restProps}
-    >
-      <svg className="embla__button__svg" viewBox="0 0 532 532">
-        <path
-          fill="currentColor"
-          d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
-        />
-      </svg>
-      {children}
-    </button>
-  );
-};
-
-const PrevArrow = ({ className, style, onClick }) => (
-  <button
-    className={className}
-    style={{ ...style, display: 'block' }}
-    onClick={onClick}
-    aria-label="previous"
-  >
-    <Icon name={leftArrowSVG} size="48px" />
-  </button>
-);
-
-const NextArrow = ({ className, style, onClick }) => (
-  <button
-    className={className}
-    style={{ ...style, display: 'block' }}
-    onClick={onClick}
-    aria-label="next"
-  >
-    <Icon name={rightArrowSVG} size="48px" />
-  </button>
-);
 
 const SliderView = (props) => {
   const {
@@ -160,8 +84,10 @@ const SliderView = (props) => {
     // This syncs the current slide with the objectwidget (or other sources
     // able to access the slider context)
     // that can modify the SliderContext (and come here via props slideIndex)
-    scrollTo(slideIndex);
-  }, [slideIndex, scrollTo]);
+    if (isEditMode) {
+      scrollTo(slideIndex);
+    }
+  }, [slideIndex, scrollTo, isEditMode]);
 
   return (
     <>
@@ -175,35 +101,36 @@ const SliderView = (props) => {
           </Message>
         )}
         {data.slides?.length > 0 && (
-          <div className="embla">
-            <div className="embla__viewport" ref={emblaRef}>
-              <div className="embla__container">
-                {data.slides &&
-                  data.slides.map((item, index) => {
-                    return (
-                      <div key={item['@id']} className="embla__slide">
-                        <Body
-                          {...props}
-                          key={item['@id']}
-                          data={item}
-                          isEditMode={isEditMode}
-                          dataBlock={data}
-                          index={index}
-                          block={block}
-                          openObjectBrowser={openObjectBrowser}
-                          onChangeBlock={onChangeBlock}
-                          isActive={selectedIndex === index}
-                        />
-                      </div>
-                    );
-                  })}
+          <>
+            <div className="embla">
+              <div className="embla__viewport" ref={emblaRef}>
+                <div className="embla__container">
+                  {data.slides &&
+                    data.slides.map((item, index) => {
+                      return (
+                        <div key={item['@id']} className="embla__slide">
+                          <Body
+                            {...props}
+                            key={item['@id']}
+                            data={item}
+                            isEditMode={isEditMode}
+                            dataBlock={data}
+                            index={index}
+                            block={block}
+                            openObjectBrowser={openObjectBrowser}
+                            onChangeBlock={onChangeBlock}
+                            isActive={selectedIndex === index}
+                          />
+                        </div>
+                      );
+                    })}
+                </div>
               </div>
               <div className="embla__buttons">
                 <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
                 <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
               </div>
             </div>
-
             <div className="embla__dots">
               {scrollSnaps.map((_, index) => (
                 <DotButton
@@ -215,7 +142,7 @@ const SliderView = (props) => {
                 />
               ))}
             </div>
-          </div>
+          </>
         )}
       </div>
     </>
