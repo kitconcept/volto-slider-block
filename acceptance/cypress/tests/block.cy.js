2b179082bab0269972bce14c2a8558edcee6ab75
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-03-07 10:30:02 +0100

    Fix cypress

--- acceptance/cypress/tests/block.cy.js
+++ acceptance/cypress/tests/block.cy.js
@@ -34,7 +34,7 @@ context('Block Acceptance Tests', () => {
     // First slide
     cy.get(
       '.objectbrowser-field[aria-labelledby="fieldset-default-field-label-href-0-slides-0"] button[aria-label="Open object browser"]',
-    ).click();
+    ).click({ multiple: true });
     cy.get('aside .breadcrumbs svg.home-icon').click();
     cy.findByLabelText('Select My Page').dblclick();
 
@@ -46,7 +46,7 @@ context('Block Acceptance Tests', () => {
     ).should('be.visible');
     cy.get(
       '.objectbrowser-field[aria-labelledby="fieldset-default-field-label-href-0-slides-1"] button[aria-label="Open object browser"]',
-    ).click();
+    ).click({ multiple: true });
     cy.get('aside .breadcrumbs svg.home-icon').click();
     cy.findByLabelText('Select My Page').dblclick();
 
